428b82efb90faac5cbb8f73d1fe715e62236a1fe
lcroysl <huanjoe@live.se>
2018-09-11 14:04:39 +0200

    Frågar namn

--- src/llol/Uppgift_v36.java
+++ src/llol/Uppgift_v36.java
@@ -7,6 +7,9 @@ public class Uppgift_v36 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("What is your name?");
+		String name = input.nextLine();
+		
 
 	}
 
