293e6ac2cc89b6034887235e6aa3f8f0e38014c0
lcroysl <huanjoe@live.se>
2018-09-11 14:07:24 +0200

    Skriver ut info

--- src/llol/Uppgift_v36.java
+++ src/llol/Uppgift_v36.java
@@ -19,7 +19,14 @@ public class Uppgift_v36 {
 		String city = input.nextLine();
 		System.out.println("What is your number?");
 		String number = input.nextLine();
-		
+
+		System.out.println("Information:");
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Address: " + address );
+		System.out.println("Post code: " + post);
+		System.out.println("City: " + city);
+		System.out.println("Phone number: " + number);
 
 	}
 
